8b2befc21031024aceeed8850e492d4cf15b1892
Guilherme <contatoguigvd@hotmail.com>
2023-01-29 22:24:35 -0300

    add skills

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,12 @@
 import html from "./tec_html.png";
 import css from "./tec_css.png";
+import js from "./tec_js.png";
+import react from "./tec_react.png";
+import angular from "./tec_angular.png";
+import git from "./tec_git.png";
+import nodejs from "./tec_nodejs.png";
+import ts from "./tec_ts.png";
+import java from "./tec_java.png";
+import mysql from "./tec_mysql.png";
 
-export { html, css };
+export { html, css, js, react, angular, git, nodejs, ts, java, mysql };
--- src/data/index.js
+++ src/data/index.js
@@ -1,42 +1,69 @@
 // IMPORT IMG
-import { html, css } from "../assets";
+import { html, css, js, react, angular, git, nodejs, ts, java, mysql } from "../assets";
 
 // SKILLS
 export const skills = [
   {
     id: 1,
-    title: "JavaScript",
+    title: "Html",
     subtitle:
-      "Estudo e venho praticado JavaScript e suas funcionalidades como: fluxo de controle, estrutura de repetições, orientação a objetos, JSON, API entre outras.",
+      "Estudo e pratico HTML, essa tecnologia está presente na maioria dos meus projetos.",
     icon: html,
   },
   {
     id: 2,
-    title: "React",
+    title: "CSS",
     subtitle:
-      "Estudo e pratico a biblioteca React e suas funcionalidades como: hooks, components, React Router , forms entre outras.",
+      "Igual ao html, o css também está na maioria dos meus projetos, fazendo uso de seletores, Flexbox, responsividade entre outras.",
     icon: css,
   },
   {
     id: 3,
-    title: "TypeScript",
-    subtitle:
-      "Uso TypeScript quando preciso desenvolver aplicações mais complexas, pois tem grande potencial de detectar erros durante o desenvolvimento, tornando assim mais seguro e ágil a aplicação.",
-    icon: css,
-  },
-  {
-    id: 4,
-    title: "Angular",
-    subtitle:
-      "Comecei a estudar o framework Angular em 2022 e desde então venho praticando suas fucionalidades como: components, filter e pipes, property binding, diretivas entre outras",
-    icon: css,
-  },
- 
-  {
-    id: 5,
     title: "JavaScript",
     subtitle:
       "Estudo e venho praticado JavaScript e suas funcionalidades como: fluxo de controle, estrutura de repetições, orientação a objetos, JSON, API entre outras.",
-    icon: css,
+    icon: js,
+  },
+  {
+    id: 4,
+    title: "React",
+    subtitle:
+      "Estudo e pratico a biblioteca React e suas funcionalidades como: hooks, components, React Router , forms entre outras.",
+    icon: react,
+  },
+  {
+    id: 5,
+    title: "Angular",
+    subtitle:
+      "Comecei a estudar o framework Angular em 2022 e desde então venho praticando suas fucionalidades como: components, filter e pipes, property binding, diretivas entre outras.",
+    icon: angular,
+  },
+  {
+    id: 6,
+    title: "TypeScript",
+    subtitle:
+      "Uso TypeScript quando preciso desenvolver aplicações mais complexas, pois tem grande potencial de detectar erros durante o desenvolvimento, tornando assim mais seguro e ágil a aplicação.",
+    icon: ts,
+  },
+  {
+    id: 7,
+    title: "Node.js",
+    subtitle:
+      "Uso o Node.js para processar, executar e desenvolver aplicações em JavaScript.",
+    icon: nodejs,
+  },
+  {
+    id: 8,
+    title: "Java",
+    subtitle:
+      "Estudo e pratico Java como linguagem de backend e suas funcionalidades como: fluxo de controle, orientação a objetos, herança, polimorfismo entre oturas.",
+    icon: java,
+  },
+  {
+    id: 9,
+    title: "MySQL",
+    subtitle:
+      "Uso o MySQL para criar bancos de dados e executar comandos como o insert, delete, update e select.",
+    icon: mysql,
   },
 ];
